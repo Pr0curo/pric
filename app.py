74623e7e3d08a5eafa90d1f2f48a75b0fcf4a215
Pr0curo <jhelbig86@gmail.com>
2022-11-25 22:27:47 +0100

    make a simple hello world example

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
-app.config.from_object("config.Config")
+
+
+def start_app() -> Flask:
+    app.config.from_object("config.Config")
+
+    return app
+
+
+@app.route("/")
+def index():
+    text = "Hello World"
+    return render_template("base.html", text=text)
+
+
+if __name__ == "__main__":
+    start_app().run()
